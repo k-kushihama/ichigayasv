4ad058d3cd89ef354cf48ff07bccbe30d0fc6292
ichigaya <133657326+ichigaya@users.noreply.github.com>
2024-01-04 20:44:16 +0900

    Add files via upload

---
+++ blog/d1di2lzuh97fh2.cloudfront.net/client.fe/js.compiled/lang.ja.1284cb9e.js
@@ -0,0 +1,14 @@
+// Copyright 2006 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+window.translations=["Free （無料）","Mini","Standard","Pro","Limited","Business","Do not show","In stock","Out of stock","Available in 3 days","Available in 7 days","Available in 14 days","Available in 1 month","Available in more than 1 month","フォトギャラリー","このギャラリーにはまだ画像がありません。","ブログ","商品","まだ記事が投稿されていません。「ブログ記事を追加」をクリックしてスタート。","まだ商品がありません。「商品を追加」をクリックして入れましょう。","まだブログ記事はありません。「ブログ」ページを追加してスタートしましょう。","このカテゴリーは空です。商品を入れるか、他のカテゴリーを選択ください。"];
